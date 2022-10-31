6a974cad16a35a6800c644bd516db0402f92d8b0
Iñigo Marin <inigomaringarcia@gmail.com>
2022-10-31 15:55:33 +0100

    copilot delete

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -63,24 +63,6 @@ lvim.plugins = {
 		run = "cd js && npm ci",
 	},
 	{ "tzachar/cmp-tabnine", run = "./install.sh" },
-	{
-		"zbirenbaum/copilot.lua",
-		-- event = { "VimEnter" },
-		config = function()
-			vim.defer_fn(function()
-				require("copilot").setup({
-					plugin_manager_path = os.getenv("LUNARVIM_RUNTIME_DIR") .. "/site/pack/packer",
-				})
-			end, 100)
-		end,
-	},
-	{
-		"zbirenbaum/copilot-cmp",
-		after = { "copilot.lua" },
-		config = function()
-			require("copilot_cmp").setup()
-		end,
-	},
 	-- Packer
 	"MunifTanjim/nui.nvim",
 	{
